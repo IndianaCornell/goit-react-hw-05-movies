39e8cf9846a8cabbd5b43d7a16bb385fb6db287b
Serj Pyshkin <IndianaCornell@gmail.com>
2023-09-15 23:39:02 +0300

    Create repo
    Create repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+import { GlobalStyle } from './GlobalStyles';
+import { Layout } from './Layout';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return ( 
+  <Layout>
+        <nav>
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/movies">Movies</Link>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={"sdadsaddsadasdaa"} />
+        <Route path="/movies" element={"sdadsada"} />
+        <Route path="/movies:movieId" element={"sdadsada"} >
+          <Route path="cast" element={"dsadasda"} />
+          <Route path="reviews" element={"dsadasda"} />
+        </Route>
+        <Route path="*" element={"dsadsada"} />
+      </Routes>
+    <GlobalStyle />
+  </Layout>)
 };
